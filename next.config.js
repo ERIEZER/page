9249eb0d082dc62b90809f266a8314b627d5cba2
David Pineda <deibittop@gmail.com>
2025-06-07 21:28:20 +0900

    Remove static export for Vercel

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
-const isProd = process.env.NODE_ENV === 'production';
+// next.config.js
+// ⛔️ これは消す → output: 'export'
+
+const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
-  output: 'export',
-  basePath: isProd ? '/page' : '',
-  assetPrefix: isProd ? '/page/' : '',
+  basePath: '', // GitHub Pages のときだけ必要だった設定
+  assetPrefix: '', // GitHub Pages 用の設定
   images: {
-    unoptimized: true,
+    unoptimized: true, // Vercel では false（または削除）
   },
-};
+}
